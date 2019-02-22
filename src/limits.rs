cabbd393610b0cf395dc6ea0704e327890b36ed4
Brad Svercl <bradsvercl@gmail.com>
2019-02-21 22:52:14 -0600

    Add triangle limits

--- src/limits.rs
+++ src/limits.rs
@@ -3,3 +3,6 @@ pub const MAX_RECTANGLE_SIZE: f64 = 10.0;
 
 pub const MIN_CIRCLE_SIZE: f64 = 0.1;
 pub const MAX_CIRCLE_SIZE: f64 = 10.0;
+
+pub const MIN_TRIANGLE_SIZE: f64 = 0.1;
+pub const MAX_TRIANGLE_SIZE: f64 = 10.0;
